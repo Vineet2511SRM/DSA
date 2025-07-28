5d730618582a89261dabd2a1368913eef975476f
Vineet2511SRM <vs4622@srmist.edu.in>
2025-07-28 22:26:10 +0530

    C

---
+++ C/Structure/SelfStructure.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+struct mem {
+    int value;
+    struct mem *next; // Pointer to the next node
+}mem1,mem2;
+
+int main(){
+    mem1.value = 10; // Assigning value to first node
+    mem1.next = NULL; // Initializing next pointer to NULL
+
+    mem2.value = 20; // Assigning value to second node
+    mem2.next = NULL; // Initializing next pointer to NULL
+
+    // Linking the first node to the second node
+    mem1.next = &mem2;
+
+    // Displaying the values of the linked nodes
+    printf("Value of first node: %d\n", mem1.value);
+    printf("Value of second node: %d\n", mem1.next->value);
+
+    return 0; // Program end
+}
